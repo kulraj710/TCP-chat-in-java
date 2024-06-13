258219505c4f59dbc794ae9b8079f1a54a04e14e
kulraj chavda <kulraj710@gmail.com>
2024-06-13 16:18:41 +0530

    Update : basic set up

--- Server.java
+++ Server.java
@@ -1,7 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server implements Runnable {
 
     @Override
     public void run() {
+        try {
+            ServerSocket server = new ServerSocket(3000);
+            Socket client = server.accept();
+        } catch (IOException e) {
+            // TO-DO : handle
+        }
+    }
+
+    class ConnectionHandler implements Runnable {
+
+        private Socket client;
+        private BufferedReader in;
+        private PrintWriter out;
+
+        public ConnectionHandler(Socket client){
+            this.client = client;
+        }
+
+        @Override
+        public void run() {
+            try {
+                out = new PrintWriter(client.getOutputStream(), true);
+                in =  new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+                // out.println("Hello");
         
+
+            } catch (Exception e) {
+                // TODO: handle exception
+            }
+        }
     }
 }
