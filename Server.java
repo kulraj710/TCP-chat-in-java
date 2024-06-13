8401c6b10feee1de94104946bb04319a8a4e2da5
kulraj chavda <kulraj710@gmail.com>
2024-06-13 16:50:32 +0530

    add : threadpool

--- Server.java
+++ Server.java
@@ -5,15 +5,18 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Server implements Runnable {
 
     private ArrayList<ConnectionHandler> connections;
     private ServerSocket server;
-
     private boolean done;
+    private ExecutorService pool;
 
-    public Server(){
+    public Server() {
         connections = new ArrayList<>();
         done = false;
     }
@@ -23,49 +26,45 @@ public class Server implements Runnable {
         try {
             server = new ServerSocket(3000);
 
-            while (!done) {
-                
-            
-            
-            Socket client = server.accept();
+            pool = Executors.newCachedThreadPool();
 
-            ConnectionHandler handler = new ConnectionHandler(client);
-            connections.add(handler);
+            while (!done) {
+
+                Socket client = server.accept();
+                ConnectionHandler handler = new ConnectionHandler(client);
+                connections.add(handler);
+                pool.execute(handler);
+
             }
         } catch (IOException e) {
             shutdown();
         }
     }
 
+    public void broadcast(String message) {
 
-    public void broadcast(String message){
-
-        for (ConnectionHandler ch : connections){
-            if (ch != null){
+        for (ConnectionHandler ch : connections) {
+            if (ch != null) {
                 ch.sendMessage(message);
             }
         }
     }
 
+    public void shutdown() {
+        try {
+            done = true;
+            if (!server.isClosed()) {
+                server.close();
+            }
 
+            for (ConnectionHandler ch : connections) {
+                ch.shutdown();
+            }
 
-    public void shutdown(){
-        try{
-        done = true;
-        if (!server.isClosed()){
-            server.close();
-        }
-
-        for (ConnectionHandler ch : connections){
-            ch.shutdown();
-        }
-
-        }
-        catch (IOException e){
+        } catch (IOException e) {
             // ignore exception as we can not handle it again and again
         }
 
-        
     }
 
     class ConnectionHandler implements Runnable {
@@ -75,7 +74,7 @@ public class Server implements Runnable {
         private PrintWriter out;
         private String username;
 
-        public ConnectionHandler(Socket client){
+        public ConnectionHandler(Socket client) {
             this.client = client;
         }
 
@@ -83,15 +82,16 @@ public class Server implements Runnable {
         public void run() {
             try {
                 out = new PrintWriter(client.getOutputStream(), true);
-                in =  new BufferedReader(new InputStreamReader(client.getInputStream()));
+                in = new BufferedReader(new InputStreamReader(client.getInputStream()));
 
-                // To-DO : Check Edge Cases (for now here it is assumed that the client will enter a valid username)
-                //         1.possibly check if the string is empty or too long
-                //         1.possibly check if the string does not contain any malicious or bad words
+                // To-DO : Check Edge Cases (for now here it is assumed that the client will
+                // enter a valid username)
+                // 1.possibly check if the string is empty or too long
+                // 1.possibly check if the string does not contain any malicious or bad words
                 out.println("Please Enter a Username: ");
 
                 username = in.readLine();
-        
+
                 // log connected
                 System.out.println(username + " Connected");
 
@@ -100,10 +100,10 @@ public class Server implements Runnable {
                 String message;
 
                 while ((message = in.readLine()) != null) {
-                    if (message.startsWith("/user ")){
+                    if (message.startsWith("/user ")) {
                         String[] messageSplit = message.split(" ", 2);
 
-                        if (messageSplit.length == 2){
+                        if (messageSplit.length == 2) {
                             broadcast(username + " renamed themselves to " + messageSplit[1]);
                             System.out.println(username + " renamed themevesl to " + messageSplit[1]);
 
@@ -111,12 +111,12 @@ public class Server implements Runnable {
 
                             out.println("succssfully changed the username to : " + username);
 
-                        } else{
+                        } else {
                             out.println("No Username provided!");
                         }
                     }
 
-                    else if (message.startsWith(("/quit"))){
+                    else if (message.startsWith(("/quit"))) {
                         broadcast(username + " left the chat");
                         shutdown();
                     }
@@ -124,31 +124,31 @@ public class Server implements Runnable {
                     else {
                         broadcast(username + ": " + message);
                     }
-            }
-
+                }
 
             } catch (Exception e) {
                 shutdown();
             }
         }
 
-        public void sendMessage(String message){
+        public void sendMessage(String message) {
             out.println(message);
         }
 
-
-        public void shutdown(){
+        public void shutdown() {
             try {
-            in.close();
-            out.close();
+                in.close();
+                out.close();
 
-            if (!client.isClosed()){
-                client.close();
+                if (!client.isClosed()) {
+                    client.close();
+                }
+            } catch (IOException e) {
+                // ignore this execption
             }
         }
-        catch (IOException e){
-            // ignore this execption 
-        }
-        }
     }
+
+
+
 }
